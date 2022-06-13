fea5664f9ff93916b59755583e6fa656ca37a5e0
MartinQuesta <martinquesta@gmail.com>
2022-06-13 15:33:19 -0300

    agregado instancia

--- src/controllers/jobshandler.js
+++ src/controllers/jobshandler.js
@@ -1,19 +1,22 @@
-import transformhttp from '../services/httpTransformJobToMongo.js'
+import httpToMongo from '../services/httpTransformJobToMongo.js'
 
-async function getJob(){
-    const plainList = await transformhttp.find() //GENERAR UN TDD QUE ENVIE UN GET Y VERIFIQUE LA LISTA DEVUELTA
+async function getJobs(body){
+    console.log(body);
+    const userID = body.id
+    console.log(userID);
+    const plainList = await httpToMongo.find(userID) //GENERAR UN TDD QUE ENVIE UN GET Y VERIFIQUE LA LISTA DEVUELTA
     return plainList;
 }
 
 function findJob(req,res){
     const id = req.params.id
-    const response = transformhttp.findById(id,res)
+    const response = httpToMongo.findById(id,res)
     return response
 }
 async function createJob(body){
     console.log('este es el body');
     console.log(body);
-    const response = await transformhttp.createJob(body)
+    const response = await httpToMongo.createJob(body)
     console.log(response);
     return response.data
 }
@@ -21,14 +24,15 @@ async function createJob(body){
 function modifyJob(req,res){
     const id = req.params.id
     const body = req.body
-    transformhttp.findAndModif(id, body,res)        ///COMO MANEJO LOS ERRORESSSS  
+    httpToMongo.findAndModif(id, body,res)        ///COMO MANEJO LOS ERRORESSSS  
 }
 
 function findAndDel(req,res){
-    const id = req.params.id
-    transformhttp.findAndDel(id,res)
+    const id = req.query.id
+    const userID = req.query.userID
+    httpToMongo.findAndDel(userID,id,res)
 }
 
 export default {
-    getJob,findJob,createJob,modifyJob,findAndDel
+    getJobs,findJob,createJob,modifyJob,findAndDel
 }
--- src/routes/job.js
+++ src/routes/job.js
@@ -1,41 +1,46 @@
 import express from "express";
-import jobshandler from '../controllers/jobsHandler.js'
+import jobsHandler from '../controllers/jobsHandler.js'
 
 
 const Router = express.Router()
 
 Router.get('/', async (req, res) => {
 //Método GET para traer tabla
-res.json(await jobshandler.getJob())
+    await jobsHandler.getJobs(res.body)  //aislo el body del response
+    .then(data => {
+        if (!data) res.status(404).json({ error: 'Not found 404', message: `Jobs not found"` })
+        res.json(data); 
+    })
+    .catch(err => res.status(500).json({ error: 'Not found 500', message: `Jobs GET_ERROR - Not found" and error: ${err}` }))
 })
 
-Router.get('/:id', (req, res) => {
-    //const myRequest = req
-   jobshandler.findJob(req,res)
+Router.get('/:id', async (req, res) => {
+    await jobsHandler.getJobs(req.params)  //aislo el body del response
+    .then(data => {
+        if (!data) res.status(404).json({ error: 'Not found 404', message: `Jobs not found"` })
+        res.json(data); 
+        console.log(data);
+    })
+    .catch(err => res.status(500).json({ error: 'Not found 500', message: `Jobs GET_ERROR - Not found" and error: ${err}` }))
 })
 
 Router.post('/', async (req, res) => {
     const body = req.body
-    jobshandler.createJob(body)
+    jobsHandler.createJob(body)
     .then(response => {
         res.status(200).json({
             'data': response
         })
-        //console.log(response);
-        // res.json(response)
     })
-    
-    
-    // res.json(await jobshandler.createJob(request))
 });
 
 Router.patch('/:id', (req, res) => {
-    jobshandler.modifyJob(req,res)
+    jobsHandler.modifyJob(req,res)
 });
 
-Router.delete('/:id', (req, res) => {
+Router.delete('/', (req, res) => {
     console.log('ROUTE DELETE');
-    jobshandler.findAndDel(req,res)
+    jobsHandler.findAndDel(req,res)
 });
 
 export default {
